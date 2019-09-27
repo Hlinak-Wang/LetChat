3f22f68def5232506a89949d557c4f413676f77b
Shili Wang <z5135120@ad.unsw.edu.au>
2019-09-28 01:04:53 +1000

    new line

--- server/userprofile_test.py
+++ server/userprofile_test.py
@@ -1,4 +1,4 @@
-
+import objectToTest as ot
 
 def test_setname():
 
@@ -7,12 +7,12 @@ def test_setname():
     #name_first more than 50 characters but not name_last
     name_first = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
     name_last = 'abcd'
-    assert user_profile_setname(token, name_first, name_last) == 'First name more than 50 characters'
+    assert ot.user_profile_setname(token, name_first, name_last) == 'First name more than 50 characters'
 
     #name_last more than 50 characters but not name_first
     name_first = 'abcd'
     name_first = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
-    assert user_profile_setname(token, name_first, name_last) == 'Last name more than 50 characters'
+    assert ot.user_profile_setname(token, name_first, name_last) == 'Last name more than 50 characters'
 
     #name_last and name_first are both less than 50 characters
     name_first = 'asdf'
@@ -23,11 +23,11 @@ def test_setemail():
     token = 'asdf'
     #invalid email
     invalid_email = 'dffgfddfsa.com'
-    assert user_profile_setemail(token,invalid_email) == 'Invalid email'
+    assert ot.user_profile_setemail(token,invalid_email) == 'Invalid email'
 
     #email is already being used by other
     invalid_email = '1234567@gmail.com'
-    assert user_profile_setemail(token,invalid_email) == 'Email is already being used by other'
+    assert ot.user_profile_setemail(token,invalid_email) == 'Email is already being used by other'
 
 
 def test_sethandle():
@@ -36,18 +36,18 @@ def test_sethandle():
 
     #handle_str with over 20 characters
     handle_str = 'wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww' 
-    assert user_profile_sethandle(token, handle_str) = 'handle_str is more than 20 characters'
+    assert ot.user_profile_sethandle(token, handle_str) = 'handle_str is more than 20 characters'
 
     ???
 
 def test_uploadphoto():
 
-    
+
 
     pass
 
 def test_profile():
     valid_email = '1234545@abc.com'
     valid_password = 21345123
-    token = auth_login(valid_email,valid_password)
+    token = ot.auth_login(valid_email,valid_password)
     email, user_profile(token)
