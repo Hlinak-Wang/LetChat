a4ed1a7cce6a86f4609594f43fb3b393f5a3fdb0
Shili Wang <z5135120@ad.unsw.edu.au>
2019-09-29 00:18:25 +1000

    change test formate

--- server/other_test.py
+++ server/other_test.py
@@ -1,12 +1,37 @@
 import objectToTest as ot
+import pytest
+
+# This function is to create a valid account
+# and a public channel 
+# which would be used in other test_function
+def account_And_channel():
+    token = ot.auth_register('123@gmail.com', 123412, 'HHH', 'LLLL')
+    channel_id = ot.channels_create(token, 'hhhaaa', True)
+    return token, channel_id
 
 def test_standup_start():
 
-    pass
+    token, channel_id = account_And_channel()
+    channel_not_exist = channel_id - 199
+
+    with pytest.raises(ValueError):
+
+        # Channel does not exist
+        ot.standup_start(token, channel_not_exist)
+
+    with pytest.raises(AccessError):
+        
+        # The authorised user is not a member of the channel that message is within
+
 
 def test_standup_send():
 
-    pass
+
+    with pytest.raises(ValueError):
+
+
+    with pytest.raises(AccessError):
+
 
 def test_search():
 
@@ -15,4 +40,12 @@ def test_search():
 def test_userpermission_change():
 
 
-    pass
+    with pytest.raises(ValueError):
+
+        # U_id doesn't refer to a valid user
+
+        # Permission_id doesn't refer to a value permission
+
+    with pytest.raises(AccessError):
+
+        # The authorised user is not an admin or owner
