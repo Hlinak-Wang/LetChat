d70d79eea2683a6847408cd9faa0e50c6bce2951
Shili Wang <z5135120@ad.unsw.edu.au>
2019-09-29 00:18:40 +1000

    change test formate

--- server/userprofile_test.py
+++ server/userprofile_test.py
@@ -1,4 +1,5 @@
 import objectToTest as ot
+import pytest
 
 # This function is to create a valid account
 # which would be used in other test_function
@@ -12,50 +13,69 @@ def test_setname():
 
     token = account_register()
     #name_first more than 50 characters but not name_last
-    name_first = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
-    name_last = 'abcd'
-    assert ot.user_profile_setname(token, name_first, name_last) == 'First name more than 50 characters'
+    first_long = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+    first_short = 'asdfzxcv'
 
-    #name_last more than 50 characters but not name_first
-    name_first = 'abcd'
-    name_first = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
-    assert ot.user_profile_setname(token, name_first, name_last) == 'Last name more than 50 characters'
+    last_short = 'abcd'
+    last_long = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
 
-    #name_last and name_first are both less than 50 characters
-    name_first = 'asdf'
-    name_last = 'xzv'
-    
-    #assert what?
+    with pytest.rasises(ValueError):
+
+        # Name_first is more than 50 characters
+        ot.user_profile_setname(token,first_long,last_short)
+
+        # Name_last is more than 50 characters
+        ot.user_profile_setname(token, first_short, last_long)
 
 def test_setemail():
 
     token = account_register()
-    #invalid email
+    token_other = ot.auth_register('adfzcv@gmail.com', 123412, 'HHH', 'LLLL')
+    
     invalid_email = 'dffgfddfsa.com'
-    assert ot.user_profile_setemail(token,invalid_email) == 'Invalid email'
+    email_used_already = 'adfzcv@gmail.com'
 
-    #email is already being used by other
-    invalid_email = '1234567@gmail.com'
-    assert ot.user_profile_setemail(token,invalid_email) == 'Email is already being used by other'
+    with pytest.rasises(ValueError):
 
+        # Email entered is not a valid email
+        ot.user_profile_setemail(token,invalid_email)
+
+        # Email entered is already used by other
+        ot.user_profile_setemail(token,email_used_already)
 
 def test_sethandle():
 
     token = account_register()
+    handle_long = 'wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww' 
+    handle_short = 'abcd'
 
-    #handle_str with over 20 characters
-    handle_str = 'wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww' 
-    assert ot.user_profile_sethandle(token, handle_str) = 'handle_str is more than 20 characters'
+    with pytest.rasises(ValueError):
 
-    #what to do with name_last???
+        # handle_str is no more than 20 charaters
+        ot.user_profile_sethandle(token, handle_long)
+
+        # name_last is more than 50 characters
+        #what to do with name_last???
 
 def test_uploadphoto():
 
-    
+    token = account_register()
 
-    pass
+    with pytest.rasises(ValueError):
+
+        # img_url is returns an HTTP status other than 200
+        ot.user_profile_uploadphoto(token, img, x_start, y_start, x_end, y_end)
+
+        # x_start, y_start, x_end, y_end 
+        # are all within the dimensions of the image at the URL.
+        ot.user_profile_uploadphoto(token, img, x_start, y_start, x_end, y_end)
 
 def test_profile():
 
     token = account_register()
-    # where can we get u_id??
+    invalid_u_id = 1234567
+
+    with pytest.rasises(ValueError):
+
+        # user with u_id is not a valid user
+        ot.user_profile(token, invalid_u_id)
