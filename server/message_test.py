5aca04fc2ede674e4983fa0c8964cb853650a771
Shili Wang <z5135120@ad.unsw.edu.au>
2019-10-26 16:26:25 +1100

    bug-fix

--- server/message_test.py
+++ server/message_test.py
@@ -1,76 +1,24 @@
 import datetime
 from server.message_function import fun_send_late, fun_send, fun_remove, fun_edit, fun_react, fun_unreact, fun_pin, \
     fun_unpin
-
+from server.auth_function import register
+from server.channel_function import ch_create, ch_join
 
 # initial state of testing
 def getdata():
     data = {
-        'users': [
-            {'u_id': 123,
-             'name_first': 'test',
-             'name_last': 'test',
-             'token': '12345',
-             'handle_str': 'testtest',
-             'email': 'test@test.com',
-             'password': 'test',
-             'permission_id': 1,
-             'channel_involve': [1]  # channel_id
-             },
-            {'u_id': 1234,
-             'name_first': 'test2',
-             'name_last': 'test2',
-             'token': '123456',
-             'handle_str': 'testtest2',
-             'email': 'test2@test2.com',
-             'password': 'test2',
-             'permission_id': 3,
-             'channel_involve': [1]  # channel_id
-             },
-            {'u_id': 12345,
-             'name_first': 'not in channel',
-             'name_last': 'test',
-             'token': '1234567',
-             'handle_str': 'not in channel',
-             'email': 'tests2@tests2.com',
-             'password': 'tesst2',
-             'permission_id': 3,
-             'channel_involve': []  # channel_id
-             }
-        ],
-        'channels': [{
-            'name': 'test',
-            'channel_id': 1,
-            'user_list': [
-                {'u_id': 123, 'name_first': 'test', 'name_last': 'test', 'is_owner': True},
-                {'u_id': 1234, 'name_first': 'test2', 'name_last': 'test2', 'is_owner': False}
-            ],
-            'is_public': True,
-            'standup': {'time_finish': '1/1/1900, 1:00:00', 'u_id': None},    # time_finish
-            'messages': [
-                {
-                    'message': 'test',
-                    'u_id': 123,
-                    'reacts': [{'react_id': 1, 'u_ids': []}],
-                    'is_pinned': False,
-                    'time_created': '10/20/2019, 23:25:33',
-                    'message_id': 1,
-                    'channel_id': 1
-                },
-                {
-                    'message': 'test2',
-                    'u_id': 1234,
-                    'reacts': [{'react_id': 1, 'u_ids': []}],
-                    'is_pinned': False,
-                    'time_created': '10/20/2019, 23:24:33',
-                    'message_id': 0,
-                    'channel_id': 1
-                }
-            ]
-        }],
+        'users': [],
+        'channels': [],
         'message_counter': 0,
         'message_buffer': []
     }
+    user_chowner = register(data, 'test1@test.com', 'password', 'name_first1', 'name_last')
+    user_inch = register(data, 'test2@test.com', 'password', 'name_first2', 'name_last')
+    user_notch = register(data, 'test3@test.com', 'password', 'name_first3', 'name_last')
+    channel = ch_create(data, user_chowner['token'], 'test_channel', True)
+    ch_join(data, user_inch['token'], channel['channel_id'])
+    fun_send(data, user_inch['token'], channel['channel_id'], 'test2')
+    fun_send(data, user_chowner['token'], channel['channel_id'], 'test')
     return data
 
 
@@ -99,7 +47,7 @@ def test_send_late():
 
     # Valid input
     output = fun_send_late(data, user["token"], channel["channel_id"], 'short_message', time_valid)
-    assert output == {'message_id': 0}
+    assert output == {'message_id': 2}
 
 def test_send():
     data = getdata()
@@ -117,7 +65,7 @@ def test_send():
 
     # Valid input
     output = fun_send(data, user["token"], channel["channel_id"], 'short_message')
-    assert output == {'message_id': 0}
+    assert output == {'message_id': 2}
     assert channel['messages'][0]['message'] == 'short_message'
 
 
@@ -133,17 +81,6 @@ def test_remove():
 
     output = fun_remove(data, user_admin['token'], 0)
     assert output == {}
-    assert channel['messages'] == [
-        {
-            'message': 'test',
-            'u_id': 123,
-            'reacts': [{'react_id': 1, 'u_ids': []}],
-            'is_pinned': False,
-            'time_created': '10/20/2019, 23:25:33',
-            'message_id': 1,
-            'channel_id': 1
-        }
-    ]
 
 
 def test_edit():
@@ -160,24 +97,6 @@ def test_edit():
     # Valid input
     output = fun_edit(data, user_admin["token"], 0, "new_message")
     assert output == {}
-    assert channel['messages'] == [{
-        'message': 'test',
-        'u_id': 123,
-        'reacts': [{'react_id': 1, 'u_ids': []}],
-        'is_pinned': False,
-        'time_created': '10/20/2019, 23:25:33',
-        'message_id': 1,
-        'channel_id': 1
-    },
-        {
-            'message': 'new_message',
-            'u_id': 1234,
-            'reacts': [{'react_id': 1, 'u_ids': []}],
-            'is_pinned': False,
-            'time_created': '10/20/2019, 23:24:33',
-            'message_id': 0,
-            'channel_id': 1
-        }]
 
 
 def test_react():
@@ -194,15 +113,6 @@ def test_react():
     # If the next text raises an exception means the react is successfully added
     output = fun_react(data, user["token"], 1, 1)
     assert output == {}
-    assert channel['messages'][0] == {
-        'message': 'test',
-        'u_id': 123,
-        'reacts': [{'react_id': 1, 'u_ids': [123]}],
-        'is_pinned': False,
-        'time_created': '10/20/2019, 23:25:33',
-        'message_id': 1,
-        'channel_id': 1
-    }
 
     # React the message again see whether raise exception
     assert fun_react(data, user["token"], 1, 1) == {'ValueError': 'user has reacted'}
@@ -224,15 +134,7 @@ def test_unreact():
     # Unreact the message again see whether raise excepion
     output = fun_unreact(data, user["token"], 1, 1)
     assert output == {}
-    assert channel['messages'][0] == {
-        'message': 'test',
-        'u_id': 123,
-        'reacts': [{'react_id': 1, 'u_ids': []}],
-        'is_pinned': False,
-        'time_created': '10/20/2019, 23:25:33',
-        'message_id': 1,
-        'channel_id': 1
-    }
+
     # React the message again see whether raise exception
     assert fun_unreact(data, user["token"], 1, 1) == {'ValueError': 'user has not reacted'}
 
@@ -256,15 +158,6 @@ def test_pin():
     # means the message is successfully pinned
     output = fun_pin(data, user_admin['token'], 1)
     assert output == {}
-    assert channel['messages'][0] == {
-        'message': 'test',
-        'u_id': 123,
-        'reacts': [{'react_id': 1, 'u_ids': []}],
-        'is_pinned': True,
-        'time_created': '10/20/2019, 23:25:33',
-        'message_id': 1,
-        'channel_id': 1
-    }
 
     assert fun_pin(data, user_admin['token'], 1) == {"ValueError": "Message with ID message_id is already pinned"}
 
@@ -291,12 +184,3 @@ def test_unpin():
     output = fun_pin(data, user_admin['token'], 1)
     output = fun_unpin(data, user_admin['token'], 1)
     assert output == {}
-    assert channel['messages'][0] == {
-        'message': 'test',
-        'u_id': 123,
-        'reacts': [{'react_id': 1, 'u_ids': []}],
-        'is_pinned': False,
-        'time_created': '10/20/2019, 23:25:33',
-        'message_id': 1,
-        'channel_id': 1
-    }
