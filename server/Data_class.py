84dc445bc0987e72420ff5540f4873ec96222ee7
Shili Wang <weric10032u@gmail.com>
2019-11-10 01:06:15 +1100

    add class

---
+++ server/Data_class.py
@@ -0,0 +1,114 @@
+class Data:
+
+    def __int__(self, users, channels, messages):
+        self.users_group = []
+        self.channels_group = []
+        self.messages_group = []
+
+    def add_user(self, user):
+        self.users_group.append(user)
+
+    def add_channel(self, channel):
+        self.channels_group.append(channel)
+
+    def add_message(self, message):
+        self.messages_group.append(message)
+
+    def remove_message(self, message):
+        self.messages_group.remove(message)
+
+    def get_user_number(self):
+        return len(self.users_group)
+
+    def get_user(self, key, value):
+        for user in self.users_group:
+            if user.get_user_info(user, key) == value:
+                return user
+        return None
+
+    def get_channel(self, channel_id):
+        for channel in self.channels_group:
+            if channel.get_channel_id() == channel_id:
+                return channel
+        return None
+
+    def get_message(self, message_id):
+        for message in self.messages_group:
+            if message.get_message_id() == message_id:
+                return message
+        return None
+
+    def check_unique(self, key, value):
+
+        for user in self.users_group:
+            if user.get_user_info(user, key) == value:
+                return False
+        return True
+
+    def get_all_user_detail(self):
+        all_user_detail = []
+        for user in self.users_group:
+            all_user_detail.append(user.get_user_detail())
+        return all_user_detail
+
+    def get_channel_list(self, u_id):
+        channel_list = []
+        for channel in self.channels_group:
+            if u_id in channel.get_user_list():
+                channel_list.append({
+                    'name': channel.get_channel_name(),
+                    'channel_id': channel.get_channel_id()
+                })
+        return {
+            'channels': channel_list
+        }
+
+    def get_channel_list_all(self, u_id):
+        channel_list_all = []
+        for channel in self.channels_group:
+            if channel.get_is_public() or u_id in channel.get_user_list():
+                channel_list_all.append({
+                    'name': channel.get_channel_name(),
+                    'channel_id': channel.get_channel_id()
+                })
+
+        return {
+            'channels': channel_list_all
+        }
+
+    def get_channel_message(self, channel_id, start):
+        channel_message = []
+        end = start
+        for message in self.messages_group:
+            if message.get_channel_id() == channel_id:
+                channel_message = message.get_message_info()
+                end += 1
+            if end >= start + 50:
+                return {
+                    'messages': channel_message,
+                    'start': start,
+                    'end': end
+                }
+
+        return {
+            'messages': channel_message,
+            'start': start,
+            'end': -1
+        }
+
+    def message_search(self, u_id, query_str):
+
+        searching_list = []
+        for channel in self.channels_group:
+            if u_id in channel.get_user_list:
+                searching_list.append(channel.get_channel_id())
+
+        message_match = []
+        for message in self.messages_group:
+            if message.get_channel_id() in searching_list:
+                if message.get_content() == query_str:
+                    message_match.append(message.get_message_info(u_id))
+
+        return {
+            'messages': message_match,
+        }
---
+++ server/channel_class.py
@@ -0,0 +1,43 @@
+import uuid
+
+class Channel:
+    def __init__(self, channel_name, is_public, creator_id):
+        self.channel_name = channel_name
+        self.channel_id = uuid.uuid1().int
+        self.owner_list = [creator_id]
+        self.user_list = [creator_id]
+        self.is_public = is_public
+        self.standup = {'time_finish': 0, 'u_id': None}
+        self.standup_message = ''
+
+    def join_invite_channel(self, user):
+        self.user_list.append(user)
+
+    def leave_channel(self, user):
+        self.user_list.remove(user)
+
+    def add_owner(self, user_id):
+        self.owner_list.append(user_id)
+
+    def remove_owner(self, user_id):
+        self.owner_list.remove(user_id)
+
+    def get_channel_id(self):
+        return self.channel_id
+
+    def get_owner_list(self):
+        return self.owner_list
+
+    def get_user_list(self):
+        return self.user_list
+
+    def get_is_public(self):
+        return self.is_public
+
+    def get_channel_name(self):
+        return self.channel_name
+
+    def set_standup(self, time_finish, u_id):
+        self.standup['time_finish'] = time_finish
+        self.standup['u_id'] = u_id
+
