f08c11270ef3984f8dbdcefe6b4449686821cc6c
Meilin Huang <meilin.huang@student.unsw.edu.au>
2019-11-16 14:20:58 +1100

    Update channel_test.py
    Former-commit-id: 66e8faa8bec060491b79565f09b3e36164ee73b5

--- server/channel_test.py
+++ server/channel_test.py
@@ -9,11 +9,11 @@ from server.channel_function import (
 )
 from server.message_function import fun_send
 from server.auth_functions import register
-from server.extra_function import {
+from server.extra_function import (
     fun_standup_star,
     fun_standup_send,
     fun_standup_activate
-}
+)
 
 from server.Data_class import Data
 
